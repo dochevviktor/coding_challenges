45744b2d832df9e9d9b1498b3f5ce9556f75d5a0
Viktor Dochev <dochevviktor@gmail.com>
2016-02-24 13:13:46 +0200

    Added files via upload
    Simplify and convert Lisp code

---
+++ Cons.Car.py
@@ -0,0 +1,27 @@
+import math
+"""
+ The idea here is to : 
+ 1) take the number, get its binary length that is close to a power of 2
+    ex. if 'a' in binary is 11111 nearest pow of 2 is 8
+    take this bin len and use it as a pow of 2
+ 2) take the doubled 'a' and subtract with 1)'s value output
+
+"""
+a = 123456789
+print int((a*2)-(2**(math.ceil(math.log(a)/math.log(2)))))
+
+
+"""
+This is the Lisp/Schema that needed to be simplified and converted
+
+(define (f n) #this was the only thing i understood right from the start
+ (do ((d (do ((i (- n 1) (- i 1)) # crazy for loops in loops
+              (d '() (cons i d)))
+             ((< i 0) d)) # bitshift ahoy!
+         (append (cddr d) (list (car d)))))
+     ((null? (cdr d)) (car d))))
+(display (f 123456789))
+"""
+
+
+  
